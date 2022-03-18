63bdfaa7318e01da616f8ea33ca22acffb95f10a
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2022-03-18 11:54:25 +0000

    Add bdk-swift dependency

--- Package.swift
+++ Package.swift
@@ -5,21 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "BDGWalletBDK",
+    platforms: [
+           .macOS(.v12), .iOS(.v13),
+        ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BDGWalletBDK",
             targets: ["BDGWalletBDK"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/bitcoindevkit/bdk-swift", from: "0.2.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BDGWalletBDK",
-            dependencies: []),
+            dependencies: [.product(name: "BitcoinDevKit", package: "bdk-swift")],
+            path: "Sources"),
     ]
 )
--- Sources/BDGWalletBDK/BDGWalletBDK.swift
+++ Sources/BDGWalletBDK/BDGWalletBDK.swift
@@ -1,4 +1,13 @@
-public struct BDGWalletBDK {
+//  Created by Daniel Nordh on 2/17/22.
+//
+//  The following repos have been useful in the creation of this code:
+//  - BDKSwiftSample by FuturePaul, https://github.com/futurepaul/BdkSwiftSample, which in turn was inspired by
+//  - BDK Android Demo Wallet by Thunderbiscuit, https://github.com/thunderbiscuit/bitcoindevkit-android-sample-app
+
+import Foundation
+import BitcoinDevKit
+
+public class BDGWalletBDK {
     public private(set) var text = "Hello, World!"
 
     public init() {
