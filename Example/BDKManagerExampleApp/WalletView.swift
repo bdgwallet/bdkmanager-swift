6d2c8bb581dac73527577641b136a928f8d5f187
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2022-03-25 10:23:33 +0000

    Update Example app

--- Example/BDKManagerExampleApp/WalletView.swift
+++ Example/BDKManagerExampleApp/WalletView.swift
@@ -12,17 +12,19 @@ struct WalletView: View {
     @EnvironmentObject var bdkManager: BDKManager
     
     var body: some View {
-        Text("Hello, wallet!")
-            .padding()
-        Text("Generated address")
-        Text(bdkManager.wallet?.getNewAddress() ?? "-")
-        switch bdkManager.syncState {
-        case .synced:
-            Text("Balance: \(bdkManager.balance)")
-        case .syncing:
-            Text("Syncing node")
-        default:
-            Text("Node not synced")
+        VStack (spacing: 50){
+            Text("Hello, wallet!")
+            switch bdkManager.syncState {
+            case .synced:
+                Text("Balance: \(bdkManager.balance)")
+            case .syncing:
+                Text("Balance: Syncing")
+            default:
+                Text("Balance: Not synced")
+            }
+            Text(bdkManager.wallet?.getNewAddress() ?? "-")
+        }.onAppear {
+            bdkManager.sync()
         }
     }
 }
