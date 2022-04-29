a4c4ffb2a396952ee09997a5d6ecaf8844d82ad9
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2022-04-29 13:11:17 +0100

    Temp use of master head

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["BDKManager"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/bdgwallet/bdk-swift", from: "0.2.0"),
+        .package(url: "https://github.com/bdgwallet/bdk-swift"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
